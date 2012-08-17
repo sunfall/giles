7d1816437ed177c7346fe11dc8ea9e8c592d9457
Phil Bordelon <phil@thenexusproject.org>
2012-08-17 18:44:43 -0500

    Start at a server object.
    Like the player object, this is super-minimal right now, and only
    supports adding and removing players.

---
+++ giles/server.py
@@ -0,0 +1,34 @@
+# Giles: server.py
+# Copyright 2012 Phil Bordelon
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import player
+
+class Server(object):
+    """The Giles server itself.  Tracks all players, games in progress,
+    and so on.
+    """
+
+    def __init__(self, name="library-alpha"):
+        self.name = name
+        self.players = []
+
+    def add_player(self, player):
+        if player not in self.players:
+            self.players.append(player)
+
+    def remove_player(self, player):
+        if player in self.players:
+            self.players.remove(player)
