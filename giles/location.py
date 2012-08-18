42b4767fa64acb0a0f055de267b2205af167c841
Phil Bordelon <phil@thenexusproject.org>
2012-08-17 21:27:30 -0500

    Start of a basic location class.
    This will soon handle people joining and parting a location.

---
+++ giles/location.py
@@ -0,0 +1,39 @@
+# Giles: location.py
+# Copyright 2012 Phil Bordelon
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+class Location(object):
+    """A location on Giles.  People are informed when others leave and join
+    this location, and new ones are instantiated at will.
+    """
+
+    def __init__(self, name):
+        self.name = name
+        self.players = []
+
+    def add_player(self, player):
+        if player not in self.players:
+            
+            for other in self.players:
+                other.client.send_cc("^Y%s^~ has joined the room.\n" % player.name)
+            self.players.append(player)
+
+    def remove_player(self, player):
+        if player in self.players:
+            self.players.remove(player)
+
+        for other in self.players:
+            other.client.send_cc("^Y%s^~ has left the room.\n" % player.name)
+
