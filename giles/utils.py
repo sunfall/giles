d0ac312de9b48a78f92f9eb09e048131578483f5
Phil Bordelon <phil@thenexusproject.org>
2012-08-18 12:21:54 -0500

    Add my classic Struct "class."
    That's right, I embrace the lazy.

--- giles/utils.py
+++ giles/utils.py
@@ -14,6 +14,10 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+class Struct(object):
+    # Empty class, useful for making "structs."
+    pass
+
 def booleanize(msg):
     # This returns:
     # -1 for False
