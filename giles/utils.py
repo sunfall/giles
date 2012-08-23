bab84b12fff3a8caff40f04ff192a76d8f3f975f
Phil Bordelon <phil@thenexusproject.org>
2012-08-22 22:07:38 -0500

    utils: New major utility, demangle_move().
    This function does most of the nasty gubbins of getting a meaningful
    move (or sets of moves) out of a string like "a3-b6" or "a3, b6, c5".

--- giles/utils.py
+++ giles/utils.py
@@ -34,3 +34,65 @@ def booleanize(msg):
         return -1
 
     return 0
+
+LETTERS = "abcdefghijklmnopqrstuvwxyz"
+def demangle_move(move_list):
+
+    # Most games take a move in the format Ln, where L is a letter
+    # and n is some number.  They make take more than one such move,
+    # for games where you move a piece from a to b.  Rather than
+    # force every game to reimplement all the various ways players
+    # might want to enter this, this function does the best it can
+    # to return a valid set of coordinates.  It assumes zero-based
+    # indexing, letter first; you may have to reverse entries, etc.
+    # to get it how you want, but at least you don't have to parse
+    # wacky strings.  This function will properly handle:
+    # * a2
+    # * a2 b2
+    # * a2-b2
+    # * a2,b2
+    # * a2/b2
+    # * a2, b2
+    # ...and further coordinated versions of the same, including
+    # mixed versions, ones with too many dashes, and so on.  It
+    # returns a list of tuples representing the 0-indexed
+    # letter/number sets, and a None if any of the entries were
+    # nonsense ("aa5", "0", "b", et cetera).
+
+    # Okay, so, to start, let's concatenate whatever list of
+    # strings we got...
+    move_str = " ".join(move_list)
+
+    # ...and then replace every random character we see in the
+    # string with a space.
+    new_str = ""
+    for char in move_str:
+        if char.isalnum():
+            new_str += char
+        else:
+            new_str += " "
+
+    # Now we lowercase it and split it back out into components.
+    new_str_bits = new_str.lower().split()
+
+    # Now let's work on each of the bits...
+    to_return = []
+    for bit in new_str_bits:
+
+        # Is it at least two and at most three characters long?
+        if len(bit) < 2 or len(bit) > 3:
+            return None
+
+        # Is the first bit a letter and the rest a number?
+        if not bit[0].isalpha() or not bit[1:].isdigit():
+            return None
+
+        # Okay, good.  Let's get the values.  Remember that it's zero-based,
+        # so the number needs to be decremented by one.
+        letter_val = LETTERS.index(bit[0])
+        number_val = int(bit[1:]) - 1
+
+        to_return.append((letter_val, number_val))
+
+    # Since we never bailed due to bad data, let's return the list.
+    return to_return
