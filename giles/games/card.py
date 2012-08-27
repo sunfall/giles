0405d901544209d15a313415793a3aae76b9712b
Phil Bordelon <phil@thenexusproject.org>
2012-08-27 18:03:54 -0500

    card: Adjust whitespace.
    Trivial.

--- giles/games/card.py
+++ giles/games/card.py
@@ -2,5 +2,4 @@ class Card(object):
     """A generic Card, which can be used to contain any informaiton or properties in the so-named instance variable."""
 
     def __init__(self, p = None):
-        self.properties=p
-
+        self.properties = p
