766d61729c9da87bf0d438b0b2f02c6d78a728fe
Phil Bordelon <phil@thenexusproject.org>
2012-08-27 19:01:16 -0500

    card/hand/playing_card: Add license info.
    These are under the AGPLv3+ just like the rest of the code.

--- giles/games/card.py
+++ giles/games/card.py
@@ -1,3 +1,19 @@
+# Giles: card.py
+# Copyright 2012 Rob Palkowski
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 class Card(object):
     """A generic Card, which can be used to contain any informaiton or properties in the so-named instance variable."""
 
