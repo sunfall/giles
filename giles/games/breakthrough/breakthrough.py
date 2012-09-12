f89bafedad297be0aa464b12f105ddf7129a0168
Phil Bordelon <phil@thenexusproject.org>
2012-09-12 18:32:45 -0500

    Breakthrough: Modernize.
    Breakthrough now uses the SquareGridLayout instead of an internal
    board representation, and it also now uses the useful _pre functions.
    This simplifies the code a surprising amount.
    
    Not sure I'm going to work on the other games, but it's good to know
    that the layout code is useful as-is.

--- giles/games/breakthrough/breakthrough.py
+++ giles/games/breakthrough/breakthrough.py
@@ -28,9 +28,6 @@ MAX_HEIGHT = 26
 MIN_WIDTH = 3
 MAX_WIDTH = 26
 
-BLACK = "black"
-WHITE = "white"
-
 class Breakthrough(Game):
     """A Breakthrough game table implementation.  Invented in 2000 by Dan Troyka.
     """
@@ -52,57 +49,44 @@ class Breakthrough(Game):
         self.log_prefix = "%s/%s: " % (self.table_display_name, self.game_display_name)
 
         # Breakthrough-specific stuff.
-        self.board = None
         self.width = 8
         self.height = 8
         self.turn = None
-        self.seats[0].data.side = BLACK
-        self.seats[0].data.piece_count = None
-        self.seats[1].data.side = WHITE
-        self.seats[1].data.piece_count = None
-        self.last_r = None
-        self.last_c = None
+        self.black = self.seats[0]
+        self.white = self.seats[1]
         self.resigner = None
         self.layout = None
+
+        # We cheat and create a black and white piece here.  Breakthrough
+        # doesn't differentiate between pieces, so this allows us to do
+        # comparisons later.
+        self.bp = Piece("^K", "b", "B")
+        self.bp.data.owner = self.black
+        self.wp = Piece("^W", "w", "W")
+        self.wp.data.owner = self.white
 
         self.init_board()
 
     def init_board(self):
-
-        self.board = []
-
-        # Top two rows are black.
-        self.board.append([BLACK] * self.width)
-        self.board.append([BLACK] * self.width)
-
-        # Every row other than the last two is empty.
-        for i in range(self.height - 4):
-            self.board.append([None] * self.width)
-
-        # The bottom two rows are white.
-        self.board.append([WHITE] * self.width)
-        self.board.append([WHITE] * self.width)
-
-        # Set the starting piece counts.
-        self.seats[0].data.piece_count = 2 * self.width
-        self.seats[1].data.piece_count = 2 * self.width
 
         # Create the layout and the pieces it uses as well.  Note that we
         # can be ultra-lazy with the pieces, as Breakthrough doesn't distinguish
         # between them, so there's no reason to create a bunch of identical
         # bits.
         self.layout = SquareGridLayout()
-        self.layout.resize(self.height, self.width)
-        bp = Piece("^K", "b", "B")
-        wp = Piece("^W", "w", "W")
+        self.layout.resize(self.width, self.height)
         last_row = self.height - 1
         next_last_row = last_row - 1
 
         for i in range(self.width):
-            self.layout.place(bp, 0, i)
-            self.layout.place(bp, 1, i)
-            self.layout.place(wp, next_last_row, i)
-            self.layout.place(wp, last_row, i)
+            self.layout.place(self.bp, 0, i)
+            self.layout.place(self.bp, 1, i)
+            self.layout.place(self.wp, next_last_row, i)
+            self.layout.place(self.wp, last_row, i)
+
+        # Set the piece counts.
+        self.white.data.piece_count = self.width * 2
+        self.black.data.piece_count = self.width * 2
 
     def show(self, player):
 
@@ -119,7 +103,7 @@ class Breakthrough(Game):
         if not self.turn:
             return ("The game has not yet started.\n")
 
-        if self.turn == BLACK:
+        if self.turn == self.black:
             player = self.seats[0].player
             color_msg = "^KBlack^~"
         else:
@@ -132,11 +116,11 @@ class Breakthrough(Game):
 
         seat = self.get_seat_of_player(player)
         if not seat:
-            player.tell_cc(self.prefix + "You can't move; you're not playing!\n")
+            self.tell_pre(player, "You can't move; you're not playing!\n")
             return False
 
-        if self.turn != seat.data.side:
-            player.tell_cc(self.prefix + "You must wait for your turn to move.\n")
+        if self.turn != seat:
+            self.tell_pre(player, "You must wait for your turn to move.\n")
             return False
 
         src_c, src_r = src
@@ -145,59 +129,53 @@ class Breakthrough(Game):
         dst_str = "%s%s" % (COLS[dst_c], dst_r + 1)
 
         # Make sure they're all in range.
-        if (src_r < 0 or src_r >= self.height or src_c < 0 or
-           src_c >= self.width or dst_r < 0 or dst_r >= self.height or
-           dst_c < 0 or dst_c >= self.width):
-            player.tell_cc(self.prefix + "Your move is out of bounds.\n")
+        if (not self.layout.is_valid(src_r, src_c) or 
+         not self.layout.is_valid(dst_r, dst_c)):
+            self.tell_pre(player, "Your move is out of bounds.\n")
             return False
 
         # Does the player even have a piece there?
-        if self.board[src_r][src_c] != self.turn:
-            player.tell_cc(self.prefix + "You don't have a piece at ^C%s^~.\n" % src_str)
+        src_loc = self.layout.grid[src_r][src_c]
+        if not src_loc or src_loc.data.owner != self.turn:
+            self.tell_pre(player, "You don't have a piece at ^C%s^~.\n" % src_str)
             return False
 
         # Is the destination within range?
-        if self.turn == BLACK:
+        if self.turn == self.black:
             row_delta = 1
         else:
             row_delta = -1
 
         if src_r + row_delta != dst_r:
-            player.tell_cc(self.prefix + "You can't move from ^C%s^~ to row ^R%d^~.\n" % (src_str, dst_r + 1))
+            self.tell_pre(player, "You can't move from ^C%s^~ to row ^R%d^~.\n" % (src_str, dst_r + 1))
             return False
         if abs(src_c - dst_c) > 1:
-            player.tell_cc(self.prefix + "You can't move from ^C%s^~ to column ^R%s^~.\n" % (src_str, COLS[dst_c]))
+            self.tell_pre(player, "You can't move from ^C%s^~ to column ^R%s^~.\n" % (src_str, COLS[dst_c]))
             return False
 
         # Okay, this is actually (gasp) a potentially legitimate move.  If
         # it's a move forward, it only works if the forward space is empty.
-        if src_c == dst_c and self.board[dst_r][dst_c]:
-            player.tell_cc(self.prefix + "A straight-forward move can only be into an empty space.\n")
+        if src_c == dst_c and self.layout.grid[dst_r][dst_c]:
+            self.tell_pre(player, "A straight-forward move can only be into an empty space.\n")
             return False
 
         # Otherwise, it must not have one of the player's own pieces in it.
-        elif self.board[dst_r][dst_c] == self.turn:
-            player.tell_cc(self.prefix + "A diagonal-forward move cannot be onto your own piece.\n")
+        dst_loc = self.layout.grid[dst_r][dst_c]
+        if src_loc == dst_loc:
+            self.tell_pre(player, "A diagonal-forward move cannot be onto your own piece.\n")
             return False
-
-        # This is a for-reals legitimate move.  Make it happen!
-        self.board[src_r][src_c] = None
 
         additional_str = ""
         opponent = self.seats[0]
         if seat == self.seats[0]:
             opponent = self.seats[1]
-        loc = self.board[dst_r][dst_c]
-        if loc:
+        if dst_loc:
             # It's a capture.
             additional_str = ", capturing one of ^R%s^~'s pieces" % (opponent.player)
             opponent.data.piece_count -= 1
-        self.board[dst_r][dst_c] = self.turn
-        self.channel.broadcast_cc(self.prefix + "^Y%s^~ moves a piece from ^C%s^~ to ^G%s^~%s.\n" % (seat.player, src_str, dst_str, additional_str))
-        self.last_r = dst_r
-        self.last_c = dst_c
+        self.bc_pre("^Y%s^~ moves a piece from ^C%s^~ to ^G%s^~%s.\n" % (seat.player, src_str, dst_str, additional_str))
 
-        # Also make the move on the layout.
+        # Make the move on the layout.
         self.layout.move(src_r, src_c, dst_r, dst_c, True)
 
         return ((src_r, src_c), (dst_r, dst_c))
@@ -208,9 +186,9 @@ class Breakthrough(Game):
         if (self.state.get() == "need_players" and self.seats[0].player
            and self.seats[1].player and self.active):
             self.state.set("playing")
-            self.channel.broadcast_cc(self.prefix + "^KBlack^~: ^R%s^~; ^WWhite^~: ^Y%s^~\n" %
+            self.bc_pre("^KBlack^~: ^R%s^~; ^WWhite^~: ^Y%s^~\n" %
                (self.seats[0].player, self.seats[1].player))
-            self.turn = BLACK
+            self.turn = self.black
             self.send_board()
 
     def set_size(self, player, size_bits):
@@ -229,43 +207,43 @@ class Breakthrough(Game):
             width = size_bits[0]
             height = size_bits[1]
         else:
-            player.tell_cc(self.prefix + "Invalid size command.\n")
+            self.tell_pre(player, "Invalid size command.\n")
             return
 
         if not width.isdigit() or not height.isdigit():
-            player.tell_cc(self.prefix + "Invalid size command.\n")
+            self.tell_pre(player, "Invalid size command.\n")
             return
 
         w = int(width)
         h = int(height)
 
         if w < MIN_WIDTH or w > MAX_WIDTH:
-            player.tell_cc(self.prefix + "Width must be between %d and %d inclusive.\n" % (MIN_WIDTH, MAX_WIDTH))
+            self.tell_pre(player, "Width must be between %d and %d inclusive.\n" % (MIN_WIDTH, MAX_WIDTH))
             return
 
         if h < MIN_HEIGHT or h > MAX_HEIGHT:
-            player.tell_cc(self.prefix + "Height must be between %d and %d inclusive.\n" % (MIN_HEIGHT, MAX_HEIGHT))
+            self.tell_pre(player, "Height must be between %d and %d inclusive.\n" % (MIN_HEIGHT, MAX_HEIGHT))
             return
 
         # Valid!
         self.width = w
         self.height = h
-        self.channel.broadcast_cc(self.prefix + "^R%s^~ has set the board size to ^C%d^Gx^C%d^~.\n" % (player, w, h))
+        self.bc_pre("^R%s^~ has set the board size to ^C%d^Gx^C%d^~.\n" % (player, w, h))
         self.init_board()
 
     def resign(self, player):
 
         seat = self.get_seat_of_player(player)
         if not seat:
-            player.tell_cc(self.prefix + "You can't resign; you're not playing!\n")
+            self.tell_pre(player, "You can't resign; you're not playing!\n")
             return False
 
-        if self.turn != seat.data.side:
-            player.tell_cc(self.prefix + "You must wait for your turn to resign.\n")
+        if self.turn != seat:
+            self.tell_pre(player, "You must wait for your turn to resign.\n")
             return False
 
-        self.resigner = seat.data.side
-        self.channel.broadcast_cc(self.prefix + "^R%s^~ is resigning from the game.\n" % player)
+        self.resigner = seat
+        self.bc_pre("^R%s^~ is resigning from the game.\n" % player)
         return True
 
     def handle(self, player, command_str):
@@ -287,8 +265,8 @@ class Breakthrough(Game):
                     handled = True
 
                 if primary in ("done", "ready", "d", "r",):
-                
-                    self.channel.broadcast_cc(self.prefix + "The game is now looking for players.\n")
+
+                    self.bc_pre("The game is now looking for players.\n")
                     self.state.set("need_players")
                     handled = True
 
@@ -297,7 +275,7 @@ class Breakthrough(Game):
                 if primary in ("config", "setup", "conf",):
 
                     self.state.set("setup")
-                    self.channel.broadcast_cc(self.prefix + "^R%s^~ has switched the game to setup mode.\n" % player)
+                    self.bc_pre("^R%s^~ has switched the game to setup mode.\n" % player)
                     handled = True
 
             elif state == "playing":
@@ -314,7 +292,7 @@ class Breakthrough(Game):
                         invalid = True
 
                     if invalid:
-                        player.tell_cc(self.prefix + "Invalid move command.\n")
+                        self.tell_pre(player, "Invalid move command.\n")
                     handled = True
 
                 elif primary in ("resign",):
@@ -334,31 +312,28 @@ class Breakthrough(Game):
                     else:
 
                         # Nope.  Switch turns...
-                        if self.turn == BLACK:
-                            self.turn = WHITE
-                        else:
-                            self.turn = BLACK
+                        self.turn = self.next_seat(self.turn)
 
                         # ...show everyone the board, and keep on.
                         self.send_board()
 
         if not handled:
-            player.tell_cc(self.prefix + "Invalid command.\n")
+            self.tell_pre(player, "Invalid command.\n")
 
     def find_winner(self):
 
         # If someone resigned, this is the easiest thing ever.  Same if
         # they lost all their pieces.
-        if self.resigner == WHITE or self.seats[1].data.piece_count == 0:
+        if self.resigner == self.white or self.seats[1].data.piece_count == 0:
             return self.seats[0].player_name
-        elif self.resigner == BLACK or self.seats[1].data.piece_count == 0:
+        elif self.resigner == self.black or self.seats[1].data.piece_count == 0:
             return self.seats[1].player_name
 
         # Aw, we have to do work.  If black has a piece on the last row,
         # they win; if white has a piece on the first row, they win.
-        if BLACK in self.board[-1]:
+        if self.bp in self.layout.grid[-1]:
             return self.seats[0].player_name
-        if WHITE in self.board[0]:
+        if self.wp in self.layout.grid[0]:
             return self.seats[1].player_name
 
         # ...that wasn't really much work, but there's no winner yet.
@@ -366,7 +341,7 @@ class Breakthrough(Game):
 
     def resolve(self, winner):
         self.send_board()
-        self.channel.broadcast_cc(self.prefix + "^C%s^~ wins!\n" % winner)
+        self.bc_pre("^C%s^~ wins!\n" % winner)
 
     def show_help(self, player):
 
