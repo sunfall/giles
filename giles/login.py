af4581ef5ce52f3a8e80be4f196e2304eac83a94
Phil Bordelon <phil@thenexusproject.org>
2014-01-19 10:50:17 -0500

    login.py pylint cleanup.
    This actually caught an unused import.  Woot.

--- giles/login.py
+++ giles/login.py
@@ -14,8 +14,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import player
-from state import State
+from giles.state import State
 
 class Login(object):
 
