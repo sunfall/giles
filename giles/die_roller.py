dfc7c870bae036100c74e100b4f8f0bcdb4860d7
Phil Bordelon <phil@thenexusproject.org>
2019-07-24 16:31:28 -0400

    Fix color and lack of period in die rolls.
    The result should be more obvious, and I messed up the syntax for coming
    out of color mode.

--- giles/die_roller.py
+++ giles/die_roller.py
@@ -181,6 +181,6 @@ class DieRoller(object):
 
                 # Whew.  Done!  Send it to the right people.
                 if secret:
-                    player.tell_cc("You rolled ^G%s^~ in ^Csecret^~; the result is ^!%s^. (^M%s^~)\n" % (message, str(roll_result), " ".join(die_list)))
+                    player.tell_cc("You rolled ^G%s^~ in ^Csecret^~; the result is ^Y%s^~. (^M%s^~)\n" % (message, str(roll_result), " ".join(die_list)))
                 else:
-                    player.location.notify_cc("^Y%s^~ rolled ^G%s^~; the result is ^!%s^. (^R%s^~)\n" % (player, message, str(roll_result), " ".join(die_list)))
+                    player.location.notify_cc("^Y%s^~ rolled ^G%s^~; the result is ^Y%s^~. (^R%s^~)\n" % (player, message, str(roll_result), " ".join(die_list)))
